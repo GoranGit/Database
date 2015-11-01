61d32f085658c4da8b948b33f91948d8bf030606
GoranTelerik <gorancvetkov_sr@yahoo.com>
2015-11-01 18:21:20 +0200

    "EntityFramework", "SQL Server and MySQL", "Database Modeling" and "SQL Advanced" homework added!

---
+++ Homework/09.Entity Framework/09.EntityFramework/09.EntityFramework/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customers = GetAllCustomersWithOrderMadeIn1997ShipedToCanada();
+            foreach (var item in customers)
+            {
+                Console.WriteLine(item.CompanyName);
+            }
+
+            var customers2 = GetAllCustomersWithOrderMadeIn1997ShipedToCanadaSQL();
+            foreach (var customer in customers2)
+            {
+                Console.WriteLine(customer.CompanyName);
+            }
+        }
+
+        public static IEnumerable<Customer> GetAllCustomersWithOrderMadeIn1997ShipedToCanada()
+        {
+            CustomerRepository repository = new CustomerRepository();
+            var result = repository.Customers.Where(
+                x => x.Orders.Any(y => y.OrderDate.Value.Year == 1997 && y.ShipCountry == "Canada"));
+            return result;
+        }
+
+        public static IEnumerable<Customer> GetAllCustomersWithOrderMadeIn1997ShipedToCanadaSQL()
+        {
+            string sqlQuery = "SELECT DISTINCT c.CompanyName FROM dbo.Customers c INNER JOIN dbo.Orders o ON o.CustomerID = c.CustomerID  WHERE o.OrderDate BETWEEN '1997-01-01 00:00:00' AND '1998-01-01 00:00:00' AND o.ShipCountry = 'Canada'";
+            
+            NorthwindEntities context = new NorthwindEntities();
+            var result = context.Database.SqlQuery<Customer>(sqlQuery);
+            return result;
+        }
+    }
+}
---
+++ Homework/09.Entity Framework/09.EntityFramework/09.EntityFramework/Repository.cs
@@ -0,0 +1,62 @@
+﻿namespace _09.EntityFramework
+{
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class CustomerRepository
+    {
+        private int disposeCounte;
+        private IDbSet<Customer> entities;
+        private NorthwindEntities context;
+
+        public CustomerRepository()
+        {
+            this.context = new NorthwindEntities();
+            this.disposeCounte = 0;
+        }
+
+        public IDbSet<Customer> Customers
+        {
+            get
+            {
+                if (this.entities == null)
+                {
+                    this.entities = this.context.Set<Customer>();
+                }
+
+                return this.entities;
+            }
+        }
+
+        public void Insert(Customer entity)
+        {
+
+            this.context.Customers.Add(entity);
+            this.context.SaveChanges();
+            disposeCounte++;
+
+            if (disposeCounte % 100 == 0)
+            {
+                disposeCounte = 0;
+                this.context.Dispose();
+                this.context = new NorthwindEntities();
+            }
+        }
+
+        public void Update(Customer entity)
+        {
+            context.Customers.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(Customer entity)
+        {
+            var itemToDelete = this.context.Customers.Where(x => x.CustomerID == entity.CustomerID).FirstOrDefault();
+            if (itemToDelete != null)
+            {
+                this.context.Customers.Remove(itemToDelete);
+                this.context.SaveChanges();
+            }
+        }
+    }
+}
